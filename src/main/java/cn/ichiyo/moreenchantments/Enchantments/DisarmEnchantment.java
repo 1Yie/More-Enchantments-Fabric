f37b602df0fa5d6f5e53c9b2ee16febebd14de37
ichiyo <79702446+1Yie@users.noreply.github.com>
2023-06-22 20:00:50 +0800

    fix bugs

--- src/main/java/cn/ichiyo/moreenchantments/Enchantments/DisarmEnchantment.java
+++ src/main/java/cn/ichiyo/moreenchantments/Enchantments/DisarmEnchantment.java
@@ -3,18 +3,11 @@ package cn.ichiyo.moreenchantments.Enchantments;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.*;
-import net.minecraft.entity.ai.brain.Activity;
-import net.minecraft.entity.ai.brain.Brain;
+
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Hand;
-import net.minecraft.village.TradeOfferList;
-import net.minecraft.village.VillagerData;
-import net.minecraft.world.World;
+
 
 public class DisarmEnchantment extends Enchantment {
     protected DisarmEnchantment(Rarity weight, EnchantmentTarget target, EquipmentSlot[] slotTypes) {
@@ -44,10 +37,9 @@ public class DisarmEnchantment extends Enchantment {
             ItemStack heldItem = targetEntity.getMainHandStack();
 
             if (!heldItem.isEmpty()) {
-                // 有百分之二十的概率使对方掉落物品
-                // if (user.getRandom().nextFloat() < 0.2f) {
-                ItemEntity itemEntity = new ItemEntity(targetEntity.getWorld()
-                        , targetEntity.getX(), targetEntity.getY(), targetEntity.getZ(), heldItem.copy());
+                // if (user.getRandom().nextFloat() < 0.2f) 有百分之二十的概率使对方掉落物品
+                ItemEntity itemEntity = new ItemEntity(targetEntity.getWorld(),
+                        targetEntity.getX(), targetEntity.getY(), targetEntity.getZ(), heldItem.copy());
                 targetEntity.getWorld().spawnEntity(itemEntity);
 
                 if (!(target instanceof VillagerEntity)){
