0805fbc328d892e6e7e88d8f06b7a0c54bad8538
ichiyo <79702446+1Yie@users.noreply.github.com>
2023-07-07 17:11:32 +0800

    (TEST) Test configured feature and ore placed

--- src/main/java/cn/ichiyo/moreenchantments/Data/ModRecipeGenerator.java
+++ src/main/java/cn/ichiyo/moreenchantments/Data/ModRecipeGenerator.java
@@ -7,6 +7,7 @@ import net.minecraft.data.server.recipe.RecipeJsonProvider;
 import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.util.Identifier;
 
 import java.awt.*;
 import java.util.List;
@@ -21,5 +22,16 @@ public class ModRecipeGenerator extends FabricRecipeProvider {
     public void generate(Consumer<RecipeJsonProvider> exporter) {
         offerSmelting(exporter, List.of(ItemRegister.HEALTH_BOX), RecipeCategory.MISC, ItemRegister.HEALTH_BOX,
                 3f, 300, "health_box");
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ItemRegister.DOUBLE_JUMP_ITEM)
+                .pattern("SXS")
+                .pattern(" T ")
+                .pattern(" T ")
+                .input('X', Items.GOLD_INGOT)
+                .input('S', Items.PHANTOM_MEMBRANE)
+                .input('T', Items.STICK)
+                .criterion(FabricRecipeProvider.hasItem(Items.STICK),
+                        FabricRecipeProvider.conditionsFromItem(Items.PHANTOM_MEMBRANE))
+                .offerTo(exporter, new Identifier(FabricRecipeProvider.getRecipeName(ItemRegister.DOUBLE_JUMP_ITEM)));
     }
 }
