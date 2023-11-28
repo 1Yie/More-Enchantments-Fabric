93527594f3fa5832e62ac47b3228dafdc113140d
ichiyo <ichiyo233@163.com>
2023-11-28 12:35:44 +0800

    renew: Equipment realization

--- src/main/java/cn/ichiyo/moreenchantments/Items/ModArmor/RegisterArmor.java
+++ src/main/java/cn/ichiyo/moreenchantments/Items/ModArmor/RegisterArmor.java
@@ -15,6 +15,7 @@ public class RegisterArmor {
     public static final Item DARK_STORY_MATERIAL_CHESTPLATE = new ArmorItem(RedStoneModMaterial.HEALTH_BOX, ArmorItem.Type.CHESTPLATE, new FabricItemSettings());
     public static final Item DARK_STORY_MATERIAL_LEGGINGS = new ArmorItem(RedStoneModMaterial.HEALTH_BOX, ArmorItem.Type.LEGGINGS, new FabricItemSettings());
     public static final Item DARK_STORY_MATERIAL_BOOTS = new ArmorItem(RedStoneModMaterial.HEALTH_BOX, ArmorItem.Type.BOOTS, new FabricItemSettings());
+
     public static void register() {
         Registry.register(Registries.ITEM, new Identifier(MoreEnchantments.MOD_ID, "dark_story_helmet"), DARK_STORY_MATERIAL_HELMET);
         Registry.register(Registries.ITEM, new Identifier(MoreEnchantments.MOD_ID, "dark_story_chestplate"), DARK_STORY_MATERIAL_CHESTPLATE);
